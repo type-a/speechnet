72ed191ccfe9c55f849827e1c2830cc9ca290b94
Anurag Sarkar <sarkaranurag.anjankumar2015@vit.ac.in>
2018-04-26 01:34:58 +0530

    added process

---
+++ preprocess/frame.py
@@ -0,0 +1,48 @@
+import numpy
+import scipy.io.wavfile
+from scipy.fftpack import dct
+
+# adding pre-emphasis and time-framing ...........
+def process(input_filename,pre_emphasis = 0.97,frame_size = 0.025,frame_stride = 0.01,nfilt = 40,NFFT = 512):
+    sample_rate, signal = scipy.io.wavfile.read(input_filename) 
+    signal = signal[:]
+    emphasized_signal = numpy.append(signal[0], signal[1:] - pre_emphasis * signal[:-1])
+    frame_length, frame_step = frame_size * sample_rate, frame_stride * sample_rate 
+    signal_length = len(emphasized_signal)
+    frame_length = int(round(frame_length))
+    frame_step = int(round(frame_step))
+    num_frames = int(numpy.ceil(float(numpy.abs(signal_length - frame_length)) / frame_step))  
+    pad_signal_length = num_frames * frame_step + frame_length
+    z = numpy.zeros((pad_signal_length - signal_length))
+    pad_signal = numpy.append(emphasized_signal, z)
+    indices = numpy.tile(numpy.arange(0, frame_length), (num_frames, 1)) + numpy.tile(numpy.arange(0, num_frames * frame_step, frame_step), (frame_length, 1)).T
+    frames = pad_signal[indices.astype(numpy.int32, copy=False)]
+    frames *= numpy.hamming(frame_length)
+    mag_frames = numpy.absolute(numpy.fft.rfft(frames, NFFT))  # Magnitude of the FFT
+    pow_frames = ((1.0 / NFFT) * ((mag_frames) ** 2)) # Power Spectrum
+    low_freq_mel = 0
+    high_freq_mel = (2595 * numpy.log10(1 + (sample_rate / 2) / 700))  # Convert Hz to Mel
+    mel_points = numpy.linspace(low_freq_mel, high_freq_mel, nfilt + 2)  # Equally spaced in Mel scale
+    hz_points = (700 * (10**(mel_points / 2595) - 1))  # Convert Mel to Hz
+    bin = numpy.floor((NFFT + 1) * hz_points / sample_rate)
+
+    fbank = numpy.zeros((nfilt, int(numpy.floor(NFFT / 2 + 1))))
+    for m in range(1, nfilt + 1):
+        f_m_minus = int(bin[m - 1])   
+        f_m = int(bin[m])            
+        f_m_plus = int(bin[m + 1])   
+
+        for k in range(f_m_minus, f_m):
+            fbank[m - 1, k] = (k - bin[m - 1]) / (bin[m] - bin[m - 1])
+        for k in range(f_m, f_m_plus):
+            fbank[m - 1, k] = (bin[m + 1] - k) / (bin[m + 1] - bin[m])
+    filter_banks = numpy.dot(pow_frames, fbank.T)
+    filter_banks = numpy.where(filter_banks == 0, numpy.finfo(float).eps, filter_banks) 
+    filter_banks = 20 * numpy.log10(filter_banks)
+    return filter_banks
+
+import matplotlib.pyplot as plt
+print(process('LDC93S1.wav'))
+fig = plt.figure(figsize=(10, 6))
+plt.imshow(process('LDC93S1.wav').T)
+plt.show()
